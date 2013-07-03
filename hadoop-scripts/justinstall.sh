6bb44c2c03e16403d8963b6f3351023d9dd6b341
Lee T. Tucker <ltucker@verticloud.com>
2013-07-03 13:59:15 -0700

    add lzo dependency

--- hadoop-scripts/justinstall.sh
+++ hadoop-scripts/justinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 # deal with the fuse artifacts to create a tarball
 RPM_VERSION=0.1.0
-tar -C ${WORKSPACE}/hadoop-common/hadoop-hdfs-project/hadoop-hdfs/target/native/main/native/fuse-dfs -cvzf ${WORKSPACE}/hadoop-common/hadoop-dist/target/fuse-${ARTIFACT_VERSION}.tar.gz fuse_dfs 
+tar -C ${WORKSPACE}/hadoop-common/hadoop-hdfs-project/hadoop-hdfs/target/native/main/native/fuse-dfs -cvzf ${WORKSPACE}/hadoop-common/hadoop-dist/target/fuse-${ARTIFACT_VERSION}.tar.gz fuse_dfs
 
 # convert each tarball into an RPM
 DEST_ROOT=${INSTALL_DIR}/opt/fuse-${ARTIFACT_VERSION}
@@ -66,6 +66,7 @@ fpm --verbose \
 --vendor VertiCloud \
 --provides ${RPM_NAME} \
 --replaces vcc-hadoop \
+--depends 'lzo > 2.0' \
 -s dir \
 -t rpm \
 -n ${RPM_NAME}  \
