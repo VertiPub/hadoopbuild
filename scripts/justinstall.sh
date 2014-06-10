700a6497d5837d104b9a7a5bbabdae4bd5c2c21c
alee-altiscale <alee@altiscale.com>
2014-06-10 12:06:00 -0700

    AE-644 add provide tag for libhdfs to resolve Impala yum dependency

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -18,9 +18,6 @@ fpm --verbose \
 --maintainer ops@verticloud.com \
 --vendor VertiCloud \
 --provides ${RPM_NAME} \
---provides "libhdfs.so.0.0.0()(64bit)" \
---provides "libhdfs(x86-64)" \
---provides libhdfs \
 --description "${DESCRIPTION}" \
 --replaces vcc-fuse \
 -s dir \
@@ -71,6 +68,9 @@ fpm --verbose \
 --maintainer ops@verticloud.com \
 --vendor VertiCloud \
 --provides ${RPM_NAME} \
+--provides "libhdfs.so.0.0.0()(64bit)" \
+--provides "libhdfs(x86-64)" \
+--provides libhdfs \
 --replaces vcc-hadoop \
 --depends 'lzo > 2.0' \
 -s dir \
