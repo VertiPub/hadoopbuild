8511fb97b78d5c8ae6b44e7d768059ffd7e7ff09
Abin Shahab <ashahab@altiscale.com>
2014-10-19 00:02:42 -0700

    skip javadocs for trunk

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -6,5 +6,5 @@ mvn versions:set -DnewVersion=0.4.18-${DATE_STRING}
 mvn clean test package
 
 cd ${WORKSPACE}/hadoop-common
-mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
-mvn -Pdist,docs,src,native -Dmaven.test.failure.ignore=true -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
+
+mvn -DskipTests -Pdist,src,native -Dmaven.test.failure.ignore=true -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
