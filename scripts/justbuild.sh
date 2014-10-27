3cae30b0f3b3aa19d0f18fde892264be0ee017db
Ravi Prakash <raviprak@altiscale.com>
2014-10-27 10:14:56 -0700

    Make running tests optional

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -3,8 +3,12 @@
 
 cd ${WORKSPACE}/hadoop-lzo
 mvn versions:set -DnewVersion=0.4.18-${DATE_STRING}
-mvn clean test package
+mvn clean package
 
 cd ${WORKSPACE}/hadoop-common
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
-mvn -Pdist,docs,src,native -Dmaven.test.failure.ignore=true -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
+if [ RUN_UNIT_TESTS == "true" ]; then
+  mvn -Pdist,docs,src,native -Dmaven.test.failure.ignore=true -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
+else
+  mvn -Pdist,docs,src,native -Dtar -DskipTests -Dbundle.snappy -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
+fi
