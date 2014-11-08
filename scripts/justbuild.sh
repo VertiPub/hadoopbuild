9742c6a6397b39628e7413dcf584e2de7bd0252e
Ravi Prakash <raviprak@altiscale.com>
2014-11-08 09:40:21 -0800

    Fixing tests to actually run when they are selected to run

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -7,7 +7,7 @@ mvn clean package
 
 cd ${WORKSPACE}/hadoop-common
 mvn versions:set -DnewVersion=${ARTIFACT_VERSION}
-if [ RUN_UNIT_TESTS == "true" ]; then
+if [ "$RUN_UNIT_TESTS" == "true" ]; then
   mvn -Pdist,docs,src,native -Dmaven.test.failure.ignore=true -Dtar -Dbundle.snappy  -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
 else
   mvn -Pdist,docs,src,native -Dtar -DskipTests -Dbundle.snappy -Dsnappy.lib=/usr/lib64 -Drequire.fuse=true -Drequire.snappy -Dcontainer-executor.conf.dir=/etc/hadoop clean package
